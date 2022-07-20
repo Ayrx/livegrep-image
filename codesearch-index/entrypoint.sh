2e09fff1bf483f1a834dee5142f1d86bd9d3aa93
Terry Chia <terry@ayrx.me>
2022-07-20 12:53:19 +0800

    Use relative path for github.idx.

--- codesearch-index/entrypoint.sh
+++ codesearch-index/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 mkdir -p /data
-/livegrep/bin/livegrep-github-reindex -http -user Ayrx -forks=false -out /data/github.idx
+/livegrep/bin/livegrep-github-reindex -http -user Ayrx -forks=false -out github.idx
